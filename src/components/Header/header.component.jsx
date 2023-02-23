a40c54892ad1529adb4a1a255478f0e8d8a6ba88
Vranek Bálint <115113230+Kenarv@users.noreply.github.com>
2023-02-23 12:30:47 +0100

    tailwind hozzadva
    beraktam a tailwind et a projektbe es letrehoztam vele egy buta saját componenst

---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div>
+            <div className="kg:container lg:mx-auto bg-emerald-400">
+                <h1>Robotkak</h1>
+
+            </div>
+        </div>
+    )
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import 'tachyons';
-import {robotmodels } from "./models";
-
+import "tachyons";
+import { robotmodels } from "./models";
 import TerminatorList from "./components/Terminator-list/terminator-list.component";
-
+import Header from "./components/Header/header.component";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
 root.render(
-<TerminatorList robotmodelsProp={robotmodels} />
-
+  <div>
+    <Header />
+    <TerminatorList robotmodelsProp={robotmodels} />
+  </div>
 );
-
